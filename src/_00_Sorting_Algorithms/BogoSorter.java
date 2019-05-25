9fea16959b51b2d2e2959c66de888822befda7f5
apaara11 <league@iMac.attlocal.net>
2019-05-25 11:29:43 -0700

    HeapSorter; MergeSorter; BogoSorter(IP)

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -8,33 +8,32 @@ public class BogoSorter extends Sorter {
 		type = "Bogo";
 		Random r = new Random();
 	}
-	
-	// Bogo sort is a joke sorting algorithm. It is considered the most 
+
+	// Bogo sort is a joke sorting algorithm. It is considered the most
 	// inefficient sorting algorithm while still maintaining the possibility
 	// of eventually sorting data.
-	
+
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
-	//	if yes, finished; if no, go to step 2.
+	// if yes, finished; if no, go to step 2.
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
-	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+
+	// 1. Complete the sort method using the Bogo sort algorithm.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
-		for(int i= 0; i<array.length; i++) {
-		if( i+1 < array.length) {
-		if(array[i]<=array[i+1]) {
-			;
-		}
-		else {
-		int temp= array[i];
-		array[i]= array[i+1];
-		array[i+1] = temp;
-		}
 
-		}
+		for (int i = 0; i < array.length; i++) {
+			if (i + 1 < array.length) {
+				if (array[i] <= array[i + 1]) {
+					
+				} else {
+					int temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+				}
+
+			}
 		}
 	}
 }
