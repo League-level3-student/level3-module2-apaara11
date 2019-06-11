1295953196f65650c1907de340f6a20cebcb4a11
League Student <league@wtsim17.attlocal.net>
2019-06-10 18:26:06 -0700

    MoreAlgorithms (IP)

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,58 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+
 		return 0;
+	}
+//---//
+	public static Object countPearls(List<Boolean> oysters) {
+		int pearls = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				pearls++;
+			}
+		}
+		return pearls;
+	}
+//--//
+	public static double findTallest(List<Double> peeps) {
+		double biggestValue =0.0;
+		// TODO Auto-generated method stub
+	for(int i=0; i< peeps.size();i++) {
+		if(peeps.get(i)>= biggestValue) {
+			biggestValue=peeps.get(i);
+		}
+	}
+			return biggestValue;
+	}
+
+	public static Object findLongestWord(List<String> words) {	
+		
+		return null;
+	}
+
+	public static Object containsSOS(List<String> message1) {
+		// TODO Auto-generated method stub
+		String morseCode = "...---...";
+		// TODO Auto-generated method stub
+		for(int i=0; i<message1.size();i++) {
+			if(message1.get(i).equals(morseCode)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		
+		return null;
+	}
+	public static List<String> sortScores(List<Double> results) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
