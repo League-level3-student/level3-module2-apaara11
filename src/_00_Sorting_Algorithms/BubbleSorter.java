ddb79940595544de6911d0d564966bd4af7e89ca
League Student <league@iMac-11.attlocal.net>
2019-08-25 12:31:22 -0700

    Update BubbleSorter.java

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,19 +1,33 @@
 package _00_Sorting_Algorithms;
 
-import java.util.Arrays;
-
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		Arrays.sort(array);
-		display.updateDisplay();
+
+		boolean hasSwapped = true;
+
+		while (hasSwapped == true) {
+			hasSwapped = false;
+
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] > array[i + 1]) {
+					int temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+
+					hasSwapped = true;
+				}
+			}
+			display.updateDisplay();
+		}
+
 	}
-	
+
 }
